4c0e71fe1a6b564bf55e3b72546d99c7faec673c
Patrick Bittner <anjunar@gmx.de>
2025-11-22 16:45:16 +0100

    bugfix

--- src/main/scala/com/anjunar/javafx/dsl/traits/HasNode.scala
+++ src/main/scala/com/anjunar/javafx/dsl/traits/HasNode.scala
@@ -13,13 +13,13 @@ trait HasNode {
 
   lazy val node : Node
 
-  var onKeyPressed : KeyEvent => Unit = uninitialized
+  var onKeyPressedHandler : KeyEvent => Unit = uninitialized
   
-  var onMouseClicked : MouseEvent => Unit = uninitialized
+  var onMouseClickedHandler : MouseEvent => Unit = uninitialized
 
-  var onMouseDragged: MouseEvent => Unit = uninitialized
+  var onMouseDraggedHandler: MouseEvent => Unit = uninitialized
 
-  var onMousePressed : MouseEvent => Unit = uninitialized
+  var onMousePressedHandler : MouseEvent => Unit = uninitialized
 
 }
 
@@ -40,6 +40,8 @@ object HasNode {
   def hgrow()(using h: HasNode): Priority = HBox.getHgrow(h.node)
   def hgrow_=(v: Priority)(using h: HasNode): Unit = HBox.setHgrow(h.node, v)
   
+  def mouseTransparent(using h: HasNode): Boolean = h.node.isMouseTransparent()
+  def mouseTransparent_=(v: Boolean)(using h: HasNode): Unit = h.node.setMouseTransparent(v)  
 
   def pickOnBounds(using h: HasNode): Boolean = h.node.isPickOnBounds()
   def pickOnBounds_=(v: Boolean)(using h: HasNode): Unit = h.node.setPickOnBounds(v)
@@ -48,10 +50,10 @@ object HasNode {
     h.node.addEventHandler(eventType, eventHandler)
   
   def onKeyPressed(using h: HasNode): KeyEvent => Unit =
-    h.onKeyPressed
+    h.onKeyPressedHandler
 
   def onKeyPressed_=(f: KeyEvent => Unit)(using h: HasNode): Unit = {
-    h.onKeyPressed = f
+    h.onKeyPressedHandler = f
     h.node.setOnKeyPressed((t: KeyEvent) => f(t))
   }
   
@@ -59,26 +61,26 @@ object HasNode {
   
 
   def onMouseClicked(using h: HasNode): MouseEvent => Unit =
-    h.onMouseClicked
+    h.onMouseClickedHandler
 
   def onMouseClicked_=(f: MouseEvent => Unit)(using h: HasNode): Unit = {
-    h.onMouseClicked = f
+    h.onMouseClickedHandler = f
     h.node.setOnMouseClicked((t: MouseEvent) => f(t))
   }
 
   def onMouseDragged(using h: HasNode): MouseEvent => Unit =
-    h.onMouseDragged
+    h.onMouseDraggedHandler
 
   def onMouseDragged_=(f: MouseEvent => Unit)(using h: HasNode): Unit = {
-    h.onMouseDragged = f
+    h.onMouseDraggedHandler = f
     h.node.setOnMouseDragged((t: MouseEvent) => f(t))
   }
 
   def onMousePressed(using h: HasNode): MouseEvent => Unit =
-    h.onMousePressed
+    h.onMousePressedHandler
 
   def onMousePressed_=(f: MouseEvent => Unit)(using h: HasNode): Unit = {
-    h.onMousePressed = f
+    h.onMousePressedHandler = f
     h.node.setOnMousePressed((t: MouseEvent) => f(t))
   }
 
